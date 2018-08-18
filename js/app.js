9244dabc95ee16833de48643aef3aefe34c361f8
Opeoluwa Iyi-Kuyoro <opeoluwa.iyikuyoro@gmail.com>
2018-08-18 16:53:42 +0100

    feat: Add bugs and player to game

--- js/app.js
+++ js/app.js
@@ -1,22 +1,19 @@
 class Enemy {
-    constructor() {
-        // Variables applied to each of our instances go here,
-        // we've provided one for you to get started
-
-        // The image/sprite for our enemies, this uses
-        // a helper we've provided to easily load images
+    constructor(yPosition, xPosition) {
+        this.speed = Math.ceil((Math.random() * 2) + 2);
+        this.x = xPosition;
+        this.y = yPosition;
         this.sprite = 'images/enemy-bug.png';
     }
 
-    // Update the enemy's position, required method for game
-    // Parameter: dt, a time delta between ticks
-    update() {
-        // You should multiply any movement by the dt parameter
-        // which will ensure the game runs at the same speed for
-        // all computers.
+    update(dt) {
+        if (this.x > 505) {
+            this.x = -100;
+            this.speed = Math.ceil((Math.random() * 2) + 2);
+        }
+        this.x += this.speed;
     }
 
-    // Draw the enemy on the screen, required method for game
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
@@ -24,38 +21,54 @@ class Enemy {
 
 class Player {
     constructor() {
-
+        this.x = 200;
+        this.y = 380;
+        this.sprite = 'images/char-cat-girl.png';
     }
 
     update() {
-
+        if (this.y <= -20)
+            this.y += 320;
     }
 
     render() {
-
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 
     handleInput(key) {
-
+        switch (key) {
+            case 'left':
+                if (this.x !== 0)
+                    this.x -= 100;
+                break;
+            case 'up':
+                if (this.y !== -20)
+                    this.y -= 80;
+                break;
+            case 'right':
+                if (this.x !== 400)
+                    this.x += 100;
+                break;
+            case 'down':
+                if (this.y !== 380)
+                    this.y += 80;
+                break;
+            default:
+                console.log('Wrong key pressed');
+                break;
+        }
     }
 }
 
-const enemy1 = new Enemy();
-const enemy2 = new Enemy();
-const enemy3 = new Enemy();
-const enemy4 = new Enemy();
-const enemy5 = new Enemy();
-const enemy6 = new Enemy();
+const enemy1 = new Enemy(60, -100);
+const enemy2 = new Enemy(140, -150);
+const enemy3 = new Enemy(220, -200);
+const enemy4 = new Enemy(60, -125);
+const enemy5 = new Enemy(140, -95);
+const enemy6 = new Enemy(220, -190);
 const allEnemies = [enemy1, enemy2, enemy3, enemy4, enemy5, enemy6];
 const player = new Player();
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
 
-
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
